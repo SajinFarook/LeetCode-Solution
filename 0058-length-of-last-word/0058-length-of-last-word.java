cb248a283c9232dfbdd6c3604abc35add62b87df
Sajin Farook <sajinfarook123@gmail.com>
2026-07-15 11:12:18 +0530

    Time: 1 ms (31.68%), Space: 43.2 MB (33.96%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        String[] arr=s.trim().split(" ");
+        String last=arr[arr.length-1];
+        return last.length();
+    }
+}
