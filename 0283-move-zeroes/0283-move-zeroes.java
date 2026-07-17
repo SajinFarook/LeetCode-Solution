b2a4e00cc4152fe7aeff86b9c1351868e8931ac9
Sajin Farook <sajinfarook123@gmail.com>
2026-07-17 10:29:19 +0530

    Time: 260 ms (5.07%), Space: 47.6 MB (76.06%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]==0){
+                    if(nums[i]<nums[j]||nums[i]>nums[j]){
+                        int temp=nums[i];
+                        nums[i]=nums[j];
+                        nums[j]=temp;
+                    }
+                }
+            }
+        }
+    }
+}
