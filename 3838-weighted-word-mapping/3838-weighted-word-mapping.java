8397201618f349285d376c70696939916194461f
Sajin Farook <sajinfarook123@gmail.com>
2026-07-26 20:27:09 +0530

    Time: 2 ms (93.35%), Space: 46.3 MB (93.64%) - LeetHub

---
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String mapWordWeights(String[] words, int[] weights) {
+        StringBuilder result = new StringBuilder();
+        for (String word : words) {
+            int sum = 0;
+            for (int i = 0; i < word.length(); i++) {
+                sum += weights[word.charAt(i) - 'a'];
+            }
+            int mod = sum % 26;
+            result.append((char) ('z' - mod));
+        }
+        return result.toString();
+    }
+}
